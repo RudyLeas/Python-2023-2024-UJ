df2a31b25acdb2353179a2a956c41e231e8eccc4
Kuba Lis <lisk4397@gmail.com>
2023-10-24 16:16:25 +0200

    second part to 2_12 program added

--- Zestaw II/2_12.py
+++ Zestaw II/2_12.py
@@ -1,6 +1,8 @@
-# program to print a string made out of first letters of words in a sentence
+# program to print a string made out of first and last letters of words in a sentence
 def func(sentence):
     return ''.join([word[0] for word in sentence.split()])
-
+def func2(sentence):
+    return ''.join([word[-1] for word in sentence.split()])
 line = 'I am a cat and I have a ling whiskers and furry paws'
 print(func(line))
+print(func2(line))
